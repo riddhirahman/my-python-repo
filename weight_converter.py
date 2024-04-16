70133c17fcff2d33c338665d937d5cc2216bc2d0
riddhirahman <151963687+riddhirahman@users.noreply.github.com>
2024-04-16 22:30:59 +0530

    Update weight_converter.py

--- weight_converter.py
+++ weight_converter.py
@@ -5,17 +5,20 @@ convert_to =  input('To convert to L(bs) or (K)g? ')
 if choice == 'L' or choice == 'l':
     if convert_to == 'L' or convert_to == 'l':
         converted_weight = weight
+        print(f'Your converted weight is {converted_weight} pounds')
     elif convert_to == 'K' or convert_to == 'k':
         converted_weight = weight * 0.45
+        print(f'Your converted weight is {converted_weight} kilos')
     else:
         print('Invalid choice')
 elif choice == 'K' or choice == 'k':
     if convert_to == 'K' or convert_to == 'k':
         converted_weight = weight
+        print(f'Your converted weight is {converted_weight} kilos')
     elif convert_to == 'L' or convert_to == 'l':
         converted_weight = weight // 0.45
+        print(f'Your converted weight is {converted_weight} pounds')
     else:
         print('Invalid choice')
 else:
     print('Invalid choice')
-print('Your converted weight is', converted_weight)
